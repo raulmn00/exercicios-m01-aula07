a9b043a38e6ed08ad8c05bbbbd03ccdb4a8f664a
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-05-31 20:35:50 -0300

    resolucao do exercicio 01

---
+++ ex03.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")();
+console.clear();
+
+let num;
+
+let contador = 0;
+let arrayNum = [];
+
+while (contador < 20) {
+  num = Math.floor(Math.random() * 100 + 1);
+  arrayNum.push(num);
+  contador++;
+}
+console.log(arrayNum);
+console.log("Antes do metodo sort.");
+arrayNum.sort();
+console.log("Depois do metodo sort.");
+console.log(arrayNum);
