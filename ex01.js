486b070cccefce5c68875c45467b6b9bc6e99224
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-05-31 20:22:33 -0300

    resolucao do exercicio 01

--- ex01.js
+++ ex01.js
@@ -1,2 +1,24 @@
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 console.clear();
+
+let numeroInteiro = +prompt("Digite um número inteiro: ");
+
+let arrayTodo = [];
+let arrayPar = [];
+let arrayImpar = [];
+
+for (let index = 0; index <= numeroInteiro; index++) {
+  arrayTodo[index] = index;
+}
+
+for (let i = 0; i < arrayTodo.length; i++) {
+  if (arrayTodo[i] % 2 == 0) {
+    arrayPar.push(arrayTodo[i]);
+  } else {
+    arrayImpar.push(arrayTodo[i]);
+  }
+}
+
+console.log("Array completo: ", arrayTodo);
+console.log("Array com ímpares: ", arrayImpar);
+console.log("Array com pares: ", arrayPar);
